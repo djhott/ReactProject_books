93ff0116bfe9e9e380d9cbaba2f96e78993f1c71
djhott <doughott@gmail.com>
2024-08-28 11:43:57 -0500

    Added props passing for whena Book is displayed

--- src/index.js
+++ src/index.js
@@ -5,33 +5,29 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import './index.css'
+const author = 'Jordan Moore'
+const title = 'Interesting facts for Curious Minds'
+const img =
+  'https://images-na.ssl-images-amazon.com/images/I/71m+Qtq+HrL._AC_UL900_SR900,600_.jpg'
 
+//props only renders what we pass, so author will not be displayed in the second Book
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book author={author} title={title} img={img} />
+      <Book title={title} img={img} />
     </section>
   )
 }
 
 //Book component is used in BookList, className "book" used for CSS
-const author = 'Jordan Moore'
-const Book = () => {
-  const title = 'Interesting Facts For Curious Mindssssss'
+const Book = (props) => {
+  console.log(props)
   return (
     <article className="book">
-      <img
-        src="https://images-na.ssl-images-amazon.com/images/I/71m+Qtq+HrL._AC_UL900_SR900,600_.jpg"
-        alt="Interesting Facts For Curious Minds"
-      />
-      <h2>{title}</h2>
-
-      <h4>{author.toUpperCase()} </h4>
-      {/* <p>{let x = 6}</p> //statements don't work, only expressions, see next line*/}
-      {/*<p>{6 + 6}</p> //expressions because we need to return something, not statements*/}
+      <img src={props.img} alt={props.title} />
+      <h2>{props.title}</h2>
+      <h4>{props.author} </h4>
     </article>
   )
 }
